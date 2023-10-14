33b25569f5961d8fc44e21f4f04e81d95fc84e25
Melissa <melissakalish2@gmail.com>
2023-10-14 10:54:00 -0500

    changed code to reflect apollo and authmiddleware

--- server/server.js
+++ server/server.js
@@ -5,40 +5,38 @@ const db = require('./config/connection');
 const routes = require('./routes');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-// const { start } = require('repl');
 
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
-
-server.applyMiddleware({ app });
-
-
-const startApolloServer = async ( typeDefs, resolvers) =>
-await server.start();
-server.applyMiddleware({ app });
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware
   });
+
+  await server.start();
+
+  const app = express();
+  const PORT = process.env.PORT || 3001;
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
+
+  app.use(routes);
+};
+
+startServer().catch((error) => {
+  console.error('Error starting server:', error);
 });
-
-app.use(routes);
-
-startApolloServer(typeDefs, resolvers);
