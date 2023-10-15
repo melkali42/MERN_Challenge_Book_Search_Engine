3d3080362dc2f53d8bd11250df7c53ee350f5b17
Melissa <melissakalish2@gmail.com>
2023-10-15 09:40:15 -0500

    fixed error

--- server/server.js
+++ server/server.js
@@ -21,17 +21,28 @@ const startServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
+  // Serve the client's static files from the build directory
   if (process.env.NODE_ENV === 'production') {
+    // Serve index.html and other static assets
     app.use(express.static(path.join(__dirname, '../client/build')));
+
+    // For any other route, serve the index.html file
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+    });
   }
 
   server.applyMiddleware({ app });
 
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    });
+  // Database connection
+  await db.once('open', () => {
+    console.log('Connected to the database.');
+  });
+
+  // Start the server
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 
   app.use(routes);
